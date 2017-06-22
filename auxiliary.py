662ea4012f8cf68e799308db5182df73ccf782dc
Albert Brox III <albert@exypno.tech>
2017-06-22 00:05:01 -0400

    fixed spacing bug, added colorized temps

--- auxiliary.py
+++ auxiliary.py
@@ -1,3 +1,7 @@
+import colorama
+from colorama import Fore
+colorama.init()
+
 def convert_kelvin(temp, to='C', rounding=1):
     to = to.lower()
     assert to in ('f', 'c')
@@ -7,6 +11,27 @@ def convert_kelvin(temp, to='C', rounding=1):
     else:
         t = (temp * 9/5) - 459.67
     return round(t, rounding)
+
+def temp_colorizer(temp, units='C'):
+    units = units.lower()
+    assert units in ('f', 'c')
+    if units != 'f':
+        f_temp = int((temp * 9/5) + 32)
+    else:
+        f_temp = temp
+
+    if f_temp < 0:
+        color_string = Fore.BLUE + '{}' + Fore.RESET
+    elif f_temp in range(0, 25):
+        color_string = Fore.CYAN + '{}' + Fore.RESET
+    elif f_temp in range(25, 50):
+        color_string = Fore.GREEN + '{}' + Fore.RESET
+    elif f_temp in range(50, 75):
+        color_string = Fore.YELLOW + '{}' + Fore.RESET
+    elif f_temp >= 75:
+        color_string = Fore.RED + '{}' + Fore.RESET
+
+    return color_string.format(temp)
 
 
 def date_indexer(day_num):
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import datetime
 from urllib.request import urlopen
 from auxiliary import *
 
+# API_URL = \
+#     'http://api.openweathermap.org/data/2.5/forecast/daily?zip=02635,us&APPID=a42e83259c77ea994ccc6891cdf13525'
 API_URL = \
-    'http://api.openweathermap.org/data/2.5/forecast/daily?zip=02635,us&APPID=a42e83259c77ea994ccc6891cdf13525'
+    'http://api.openweathermap.org/data/2.5/forecast/daily?lat=61&lon=149&APPID=a42e83259c77ea994ccc6891cdf13525'
 
 
 def main():
@@ -22,11 +24,14 @@ def main():
         description = day['weather'][0]['description']
         description = description[0].upper() + description[1:]
 
+        temp_string = temp_colorizer(conv_temp) + "°" + (' ' * (5 - len(str(conv_temp)))) + '│ '
+
         print('─' * 11, '┼', '─' * 7, '┼', '─' * 17, sep='')
-        print(date_indexer((weekday + i) % 6), (str(conv_temp) + "° │ ") + description)
+        print(date_indexer((weekday + i) % 6), temp_string + description)
     print('─' * 11, '┴', '─' * 7, '┴', '─' * 17, sep='')
     print()
 
 
 if __name__ == '__main__':
     main()
+#(temp_colorizer(conv_temp) + "° │ ") 
