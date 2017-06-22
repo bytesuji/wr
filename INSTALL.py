2304fb5789a1023aaf8a660736fe7f1fee3d3837
Albert Brox III <albert@exypno.tech>
2017-06-22 10:50:42 -0400

    added install script

---
+++ INSTALL.py
@@ -0,0 +1,36 @@
+import os
+import pip
+from shutil import copy2
+
+
+class PrivilegeException(Exception):
+    def __init__(self, value):
+        self.paramater = value
+    def __str__(self):
+        return str(self.paramater)
+
+def install(package):
+    pip.main(['install', package])
+
+
+def main():
+    if os.geteuid() is not 0:
+        raise PrivilegeException("You must have root privileges to run this install script.")
+
+    print("Checking dependencies...")
+    try:
+        import colorama
+    except ModuleNotFoundError:
+        print("Required module [colorama] was not found. Installing via pip.")
+        install('colorama')
+
+    print("Copying files...")
+    path = '/opt/wr'
+    os.mkdir(path)
+    copy2('./main.py', path)
+    copy2('./joblib.py', path)
+    copy2('./auxiliary.py', path)
+
+    print("Creating symlink...")
+    os.symlink('/opt/wr/main.py', '/usr/local/bin/wr')
+    print("Successfully installed!")
